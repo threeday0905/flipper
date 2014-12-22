ab78edafacd8b77c9aa37f905e6a21e462bfe99a
赫门 <yuchiang.lyc@alibaba-inc.com>
2014-12-22 20:57:39 +0800

    rebuild

--- build/flipper.js
+++ build/flipper.js
@@ -2,6 +2,26 @@ window.Flipper = {
     version: '0.1.0'
 };
 
+(function() {
+    'use strcit';
+
+    var currentScriptDescriptor = {
+        get: function() {
+            var script = HTMLImports.currentScript || document.currentScript ||
+                // NOTE: only works when called in synchronously executing code.
+                // readyState should check if `loading` but IE10 is
+                // interactive when scripts run so we cheat.
+                (document.readyState !== 'complete' ?
+                    document.scripts[document.scripts.length - 1] : null);
+            return script;
+        },
+        configurable: true
+    };
+
+    Object.defineProperty(document, '_currentScript', currentScriptDescriptor);
+
+}());
+
 (function(Flipper) {
     'use strict';
 
@@ -16,6 +36,14 @@ window.Flipper = {
             callback(node, i);
         }
     };
+
+
+    utils.mix = function(to, from) {
+        Object.getOwnPropertyNames(from).forEach(function(name) {
+            Object.defineProperty(to, name,
+                Object.getOwnPropertyDescriptor(from, name));
+        });
+    };
 } (Flipper));
 
 (function(Flipper) {
@@ -26,6 +54,15 @@ window.Flipper = {
     };
 
     Flipper._config = config;
+} (Flipper));
+
+(function(Flipper) {
+    'use strict';
+
+    function parseScript() { }
+
+    Flipper.parser = Flipper.parser || {};
+    Flipper.parser.parseScript = parseScript;
 } (Flipper));
 
 (function(Flipper) {
@@ -95,24 +132,24 @@ window.Flipper = {
 
 (function(Flipper) {
     'use strict';
-
-    function parseScript() { }
-
-    Flipper.parser = Flipper.parser || {};
-    Flipper.parser.parseScript = parseScript;
-} (Flipper));
-
-(function(Flipper) {
-    'use strict';
+    var utils = Flipper.utils;
 
     var render = {
-        render: function(ele, definition) {
-            var tplText = definition.getTplHTML(),
-                xtpl = new XTemplate(tplText),
-                root = ele.createShadowRoot(),
-                html = xtpl.render();
+        xtpl: function(facotry) {
+            var tplText = facotry.getTplHTML(),
+                module = new XTemplate(tplText);
 
-            root.innerHTML = html;
+            return function(data, commands) {
+                commands = utils.mix(commands || {}, {
+                    $scope: function(scope) {
+                        return JSON.stringify(scope.getData());
+                    }
+                });
+
+                return module.render(data, {
+                    commands: commands
+                });
+            };
         }
     };
 
@@ -121,73 +158,131 @@ window.Flipper = {
 
 (function(Flipper) {
     'use strict';
-    var config = Flipper._config,
-        render = Flipper.render,
-        parser = Flipper.parser;
 
-    function registerElement(name, proto) {
+    var utils = Flipper.utils,
+        elements = {};
+
+    function tryGetNameFromCurrentScript() {
+        var script = document._currentScript,
+            element = script && script.parentNode;
+
+        return element && element.getAttribute ? element.getAttribute('name') : '';
+    }
+
+    function renderShadow(element, html) {
+        var root = element.createShadowRoot();
+        root.innerHTML = html;
+    }
+
+    function register(name, prototype) {
+        if (typeof name !== 'string') {
+            prototype = name;
+            name = tryGetNameFromCurrentScript();
+        }
+
+        if (!name) {
+            throw new Error('Element name could not be inferred.');
+        }
+
+        if (!elements[name].waitingRegister) {
+            throw new Error('Already registered prototype for element ' + name);
+        }
+
+        var element = elements[name];
+        element.waitingRegister = false;
+
+        var fetchData = prototype && prototype.fetchData,
+            overwriteRender  = prototype && prototype.render;
+
+        var eleProto = Object.create(HTMLElement.prototype, {
+            getData: {
+                value: function() {
+                    return this.data;
+                }
+            },
+            createdCallback: {
+                value: function() {
+                    var self = this;
+
+                    function render(data) {
+                        self.data = data;
+                        var html = overwriteRender ? overwriteRender.call(self, data) : element.render(data);
+                        renderShadow(self, html);
+                    }
+
+                    if (this.getAttribute('scope')) {
+                        var data = JSON.parse(this.getAttribute('scope'));
+                        render(data);
+                        this.removeAttribute('scope');
+
+                    } else if (typeof fetchData === 'function') {
+                        fetchData().then(function(data) {
+                            render(data);
+                        });
+                    } else {
+                        render({});
+                    }
+                }
+            }
+        });
+
         document.registerElement(name, {
-            prototype: proto
+            prototype: eleProto
         });
     }
 
-    var sharedProto = Object.create(HTMLElement.prototype, {
-        find: {
-            value: function(selector) {
-                return this.querySelectorAll(selector);
-            }
+    Flipper.elements  = elements;
+    Flipper.register = register;
+}(Flipper));
+
+(function(Flipper) {
+    'use strict';
+    var config = Flipper._config,
+        utils  = Flipper.utils,
+        render = Flipper.render,
+        parser = Flipper.parser,
+        elements = Flipper.elements;
+
+    var factoryProto = Object.create(HTMLElement.prototype);
+    utils.mix(factoryProto, {
+        find: function(selector) {
+            return this.querySelectorAll(selector);
         },
-        getName: {
-            value: function() {
-                return this.getAttribute('name');
-            }
+        getName: function() {
+            return this.getAttribute('name');
         },
-        getTemplate: {
-            value: function() {
-                return this.querySelector('template');
-            }
+        getTemplate: function() {
+            return this.querySelector('template');
         },
-        getTplContent: {
-            value: function() {
-                var tpl = this.getTemplate();
-                return tpl ? tpl.content : '';
-            }
+        getTplContent: function() {
+            var tpl = this.getTemplate();
+            return tpl ? tpl.content : '';
         },
-        getTplHTML: {
-            value: function() {
-                var tpl = this.getTemplate();
-                return tpl ? tpl.innerHTML : '';
-            }
+        getTplHTML: function() {
+            var tpl = this.getTemplate();
+            return tpl ? tpl.innerHTML : '';
         },
-        getOwner: {
-            value: function() {
-                return this.ownerDocument;
-            }
+        getOwner: function() {
+            return this.ownerDocument;
         }
     });
 
-    var factoryProto = Object.create(sharedProto, {
-        createdCallback: {
-            value: function() {
-                var self = this,
-                    name = this.getName();
+    utils.mix(factoryProto, {
+        createdCallback: function() {
+            var name = this.getName();
+            parser.parseStyle(this);
 
-                parser.parseStyle(this);
-
-                registerElement(name, Object.create(sharedProto, {
-                    createdCallback: {
-                        value: function() {
-                            render.render(this, self);
-                        }
-                    }
-                }));
-            }
+            //console.log('init ' + name);
+            elements[name] = {
+                waitingRegister: true,
+                render: render.xtpl(this)
+            };
         }
     });
 
-    var component = registerElement(config.tagName, factoryProto);
-
-    Flipper.element = component;
+    document.registerElement(config.tagName, {
+        prototype: factoryProto
+    });
 }(Flipper));
 
 (function(Flipper) {
@@ -213,9 +308,7 @@ window.Flipper = {
         }).forEach(add);
     };
 
-    Flipper.fetch = function(url) {
-        $.getJSON(url).then(function(data) {
-            console.log(data);
-        });
+    Flipper.get = function(url) {
+        return $.getJSON(url);
     };
 } (Flipper));
