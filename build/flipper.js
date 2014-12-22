cef3346fb472b6fce09a80054f87116a54c0e2b5
赫门 <yuchiang.lyc@alibaba-inc.com>
2014-12-23 02:08:16 +0800

    add polyfill todo and comments

--- build/flipper.js
+++ build/flipper.js
@@ -1,3 +1,11 @@
+/*
+    TODO Polyfills:
+    - all es5 feature (Object.keys, Array.isArray, Object.create, etc.)
+    - current script
+    - Promise
+    - web components
+ */
+
 (function() {
     'use strict';
 
