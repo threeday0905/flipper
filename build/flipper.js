ba53a100e312bff6462d41c1b092f9beeb96115b
赫门 <yuchiang.lyc@alibaba-inc.com>
2014-12-29 16:04:18 +0800

    add draft import helper

--- build/flipper.js
+++ build/flipper.js
@@ -477,20 +477,55 @@ document.registerElement('web-component', {
     })
 });
 
-/*jshint -W024 */
-Flipper.import = function(components, folder) {
-    folder = folder || '../src';
 
-    function add(name) {
-        var link = document.createElement('link');
-        link.rel = 'import';
-        link.href = folder + name + '/index.html';
-        document.head.appendChild(link);
+var packages = {};
+
+function endsWtih(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+function getPackage(str) {
+    var match = /(\w+)\//.exec(str);
+    return match ? match[1] : '';
+}
+
+Flipper.config = function(name, options) {
+    if (name === 'packages' && typeof options === 'object') {
+        Object.keys(options).forEach(function(key) {
+            packages[key] = options[key];
+        });
     }
+};
 
-    components.split(',').map(function(val) {
-        return val.trim();
-    }).forEach(add);
+Flipper.imports = function() {
+    var baseURI = document.baseURI,
+        components = Array.prototype.slice.call(arguments, 0);
+
+    if (components) {
+        var frag = document.createDocumentFragment();
+
+        components.map(function(name) {
+            var pkg = getPackage(name);
+
+            if (pkg && packages[pkg] && packages[pkg].base) {
+                name = packages[pkg].base + name.substr(pkg.length);
+            }
+
+            if ( endsWtih(name, '/') ) {
+                name += 'index.html';
+            }
+
+
+            return new URL(name, baseURI).toString();
+        }).forEach(function(url) {
+            var link = document.createElement('link');
+            link.rel = 'import';
+            link.href = url;
+            frag.appendChild(link);
+        });
+
+        document.head.appendChild(frag);
+    }
 };
 
 Flipper.findShadow = function(target, selector) {
